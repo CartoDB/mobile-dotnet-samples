6b8d7b00f77c53d705978effdf6dab1fc847d35b
mtehver <mark@nutiteq.ee>
2016-07-06 16:52:43 +0300

    Working Windows Phone 10 sample, removed legacy non-working Windows Phone 8.1 sample

--- hellomap-winphone10/Properties/AssemblyInfo.cs
+++ hellomap-winphone10/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("hellomap-dotnet")]
+[assembly: AssemblyTitle("hellomap-winphone10")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("hellomap-dotnet")]
-[assembly: AssemblyCopyright("Copyright © Nutiteq 2015")]
+[assembly: AssemblyProduct("hellomap-winphone10")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
