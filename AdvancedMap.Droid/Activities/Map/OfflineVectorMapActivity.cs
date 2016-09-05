f748000c8677cc67c02f6a4e8e514bf7d240bfc3
Aare Undo <aare@carto.com>
2016-09-05 14:04:43 +0300

    style & language fix android

--- AdvancedMap.Droid/Activities/Map/OfflineVectorMapActivity.cs
+++ AdvancedMap.Droid/Activities/Map/OfflineVectorMapActivity.cs
@@ -5,6 +5,7 @@ using Android.App;
 using Shared.Droid;
 using Shared;
 using Carto.DataSources;
+using Carto.Core;
 
 namespace AdvancedMap.Droid
 {
@@ -16,32 +17,8 @@ namespace AdvancedMap.Droid
 		{
 			base.OnCreate(savedInstanceState);
 
-			// Create package manager folder (Platform-specific)
-			var packageFolder = new File(GetExternalFilesDir(null), "packages");
-
-			if (!(packageFolder.Mkdirs() || packageFolder.IsDirectory))
-			{
-				Log.Fatal("Could not create package folder!");
-			}
-
-			// Copy bundled tile data to file system, so it can be imported by package manager
-			string importPackagePath = new File(GetExternalFilesDir(null), "world_ntvt_0_4.mbtiles").AbsolutePath;
-
-			using (var input = Assets.Open("world_ntvt_0_4.mbtiles"))
-			{
-				using (var output = new System.IO.FileStream(importPackagePath, System.IO.FileMode.Create))
-				{
-					input.CopyTo(output);
-				}
-			}
-
-			// Initialize map
-			//string downloadArea = "bbox(-0.8164,51.2382,0.6406,51.7401)"; // London (about 30MB)
-			string downloadId = "EE"; // one of ID-s from https://developer.nutiteq.com/guides/packages
-
-			// decice what to download offline
-			var toBeDownloaded = downloadId;
-			MapSetup.InitializePackageManager(packageFolder.AbsolutePath, importPackagePath, MapView, toBeDownloaded);
+			MapView.Options.ZoomRange = new MapRange(0, 6);
+			MapView.SetZoom(3, 0);
 		}
 
 		protected override TileDataSource CreateTileDataSource()
