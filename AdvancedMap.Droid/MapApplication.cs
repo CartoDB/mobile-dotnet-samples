d9da91c189456673c34ba0cccf5dde7d17327d70
Aare Undo <aare@carto.com>
2016-10-11 15:11:50 +0300

    license cleanup

--- AdvancedMap.Droid/MapApplication.cs
+++ AdvancedMap.Droid/MapApplication.cs
@@ -12,7 +12,6 @@ namespace AdvancedMap.Droid
 	public class MapApplication : Application
 	{
 		const string LICENSE = "XTUN3Q0ZFMmhzUWEwdlZwNlJNVW5kL1hMRExMYXNYVUxBaFFhKzlSK2drYjEzNnBhSTJzY1c5aDBkL2licFE9PQoKcHJvZHVjdHM9c2RrLXhhbWFyaW4tYW5kcm9pZC00LioKcGFja2FnZU5hbWU9Y29tLmNhcnRvLmFkdmFuY2VkbWFwLnhhbWFyaW4uZHJvaWQKd2F0ZXJtYXJrPWRldmVsb3BtZW50CnZhbGlkVW50aWw9MjAxNi0wOS0xOApvbmxpbmVMaWNlbnNlPTEK";
-		//const string LICENSE = "XTUN3Q0ZEeHN5b2dXLzM4WWtCQ1lZYU8zVkVOcEU0dUdBaFJYQnJzSFNTQlhqYTNIa2FLTkVRLzJtRzBLUWc9PQoKYXBwVG9rZW49MjEwYzQ4ZTAtNWVjOC00NzQyLWIwY2EtOWU4YjcyNGZmMmYwCnBhY2thZ2VOYW1lPWNvbS5jYXJ0by5jYXJ0b21hcC54YW1hcmluLmRyb2lkCm9ubGluZUxpY2Vuc2U9MQpwcm9kdWN0cz1zZGsteGFtYXJpbi1hbmRyb2lkLTQuKgp3YXRlcm1hcms9Y2FydG9kYgo=";
 
 		public MapApplication(IntPtr a, JniHandleOwnership b) : base(a, b)
 		{
@@ -32,5 +31,6 @@ namespace AdvancedMap.Droid
 			MapView.RegisterLicense(LICENSE, ApplicationContext);
 		}
 	}
+
 }
 
