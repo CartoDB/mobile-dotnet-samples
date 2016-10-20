beab992ec4beb47cd412ceca6134817cbe5b4587
Aare Undo <aare@carto.com>
2016-10-20 11:34:18 +0300

    CartoMap.iOS: appdelegate cleanup

--- CartoMap.iOS/AppDelegate.cs
+++ CartoMap.iOS/AppDelegate.cs
@@ -9,8 +9,6 @@ namespace CartoMap.iOS
 	{
 		const string License = "XTUN3Q0ZFMk5uUEwwQXQyUm5jN3BDeHpDeWdrMGs0VVRBaFFocVQybmRXRkxFcDlhUDBKWWxCN04rRHkwRWc9PQoKYXBwVG9rZW49NDQyNTRiNjQtNTRlNS00Y2Y1LTgzZDMtZDA2ZTU1M2QzOGIzCmJ1bmRsZUlkZW50aWZpZXI9Y29tLmNhcnRvLmNhcnRvbWFwCm9ubGluZUxpY2Vuc2U9MQpwcm9kdWN0cz1zZGsteGFtYXJpbi1pb3MtNC4qCndhdGVybWFyaz1jdXN0b20K";
 
-		static bool WritingViewsProgrammatically = true;
-
 		public override UIWindow Window { get; set; }
 
 		public UINavigationController Controller { get; set; }
@@ -19,26 +17,17 @@ namespace CartoMap.iOS
 		{
 			MapView.RegisterLicense(License);
 
-			if (WritingViewsProgrammatically)
-			{
-				Controller = new UINavigationController(new MapListController());
+			Controller = new UINavigationController(new MapListController());
 
-				Controller.NavigationBarHidden = false;
+			Controller.NavigationBarHidden = false;
 
-				Window = new UIWindow(UIScreen.MainScreen.Bounds);
+			Window = new UIWindow(UIScreen.MainScreen.Bounds);
 
-				Window.RootViewController = Controller;
+			Window.RootViewController = Controller;
 
-				Window.MakeKeyAndVisible();
+			Window.MakeKeyAndVisible();
 
-				Device.NavigationBarHeight = Controller.NavigationBar.Frame.Height;
-			}
-			else {
-				Carto.Utils.Log.Debug("Writing views via StoryBoard");
-
-				// Add the following KeyValuePair to Info.plist:
-				// Main storyboard file base name - Main
-			}
+			Device.NavigationBarHeight = Controller.NavigationBar.Frame.Height;
 
 			return true;
 		}
