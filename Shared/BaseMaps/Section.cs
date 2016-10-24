e1576e4d0857786ce73e8c43a56188ca92ae4b80
Aare Undo <aare@carto.com>
2016-10-24 11:57:47 +0300

    Shared: basic BaseMaps sectioning logic

---
+++ Shared/BaseMaps/Section.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shared
+{
+	public enum MapType
+	{
+		None,
+		Raster,
+		Vector
+	}
+	public class Section
+	{
+		public NameValuePair OSM { get; set; }
+
+		public MapType Type { get; set; }
+
+		public List<NameValuePair> Styles { get; set; }
+	}
+
+	public class NameValuePair 
+	{
+		public string Name { get; set; }
+
+		public string Value { get; set; }
+	}
+}
+
---
+++ Shared/BaseMaps/Sections.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shared
+{
+	public class Sections
+	{
+		public static List<Section> List
+		{
+			get
+			{
+				List<Section> sections = new List<Section>();
+
+				sections.Add(new Section
+				{
+					OSM = new NameValuePair { Name = "Nutiteq", Value = "nutiteq.osm" },
+					Type = MapType.Vector,
+					Styles = new List<NameValuePair> {
+						new NameValuePair { Name = "Bright", Value = "default" },
+						new NameValuePair { Name = "Gray", Value = "gray" },
+						new NameValuePair { Name = "Dark", Value = "dark" }
+					}
+				});
+
+				sections.Add(new Section {
+					OSM = new NameValuePair { Name = "MapZen", Value = "mapzen.osm" },
+					Type = MapType.Vector,
+					Styles = new List<NameValuePair> { 
+						new NameValuePair { Name = "Positron", Value = "positron" },
+						new NameValuePair { Name = "Dark Matter", Value = "dark_matter" }
+					}
+				});
+
+				sections.Add(new Section
+				{
+					OSM = new NameValuePair { Name = "CARTO", Value = "carto.osm" },
+					Type = MapType.Raster,
+					Styles = new List<NameValuePair> {
+						new NameValuePair { Name = "Positron", Value = "positron" },
+						new NameValuePair { Name = "Dark Matter", Value = "dark_matter" }
+					}
+				});
+
+				return sections;
+			}
+		}
+	}
+}
+
