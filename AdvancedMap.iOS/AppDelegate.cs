6f5599c0590d54768e7b1f2263f69c31b0711e6f
Aare Undo <aare@carto.com>
2016-11-01 16:40:20 +0200

    AdvancedMap.iOS: HockeyApp integration

--- AdvancedMap.iOS/AppDelegate.cs
+++ AdvancedMap.iOS/AppDelegate.cs
@@ -1,6 +1,7 @@
 ﻿
 using Carto.Ui;
 using Foundation;
+using HockeyApp.iOS;
 using Shared.iOS;
 using UIKit;
 
@@ -9,7 +10,8 @@ namespace AdvancedMap.iOS
 	[Register("AppDelegate")]
 	public class AppDelegate : UIApplicationDelegate
 	{
-		const string License = "XTUM0Q0ZRQzBoRk1OeGV5MkxqYkdHd0tqNUtINE0rN3V2QUlWQUxiQnh3eXZmaUdMakk3dXoxclExMlJvUnU3cQoKYXBwVG9rZW49NjM4NWMwNGItNTgyOC00MzJhLWIzYTEtMzI0OWU1MjY3ZDRiCmJ1bmRsZUlkZW50aWZpZXI9Y29tLmNhcnRvLmFkdmFuY2VkbWFwCm9ubGluZUxpY2Vuc2U9MQpwcm9kdWN0cz1zZGsteGFtYXJpbi1pb3MtNC4qCndhdGVybWFyaz1jdXN0b20K";
+		const string CartoLicense = "XTUM0Q0ZRQzBoRk1OeGV5MkxqYkdHd0tqNUtINE0rN3V2QUlWQUxiQnh3eXZmaUdMakk3dXoxclExMlJvUnU3cQoKYXBwVG9rZW49NjM4NWMwNGItNTgyOC00MzJhLWIzYTEtMzI0OWU1MjY3ZDRiCmJ1bmRsZUlkZW50aWZpZXI9Y29tLmNhcnRvLmFkdmFuY2VkbWFwCm9ubGluZUxpY2Vuc2U9MQpwcm9kdWN0cz1zZGsteGFtYXJpbi1pb3MtNC4qCndhdGVybWFyaz1jdXN0b20K";
+		const string HockeyId = "db59e732ee3f4b9c8bc31a2fbbea4186";
 
 		public override UIWindow Window { get; set; }
 
@@ -17,7 +19,7 @@ namespace AdvancedMap.iOS
 
 		public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
 		{
-			MapView.RegisterLicense(License);
+			MapView.RegisterLicense(CartoLicense);
 
 			UIViewController initial = new MapListController("Advanced Map Samples", Samples.RowSources);
 			Controller = new UINavigationController(initial);
@@ -35,6 +37,11 @@ namespace AdvancedMap.iOS
 			#if ENABLE_TEST_CLOUD
 			Xamarin.Calabash.Start();
 			#endif
+
+			BITHockeyManager manager = BITHockeyManager.SharedHockeyManager;
+			manager.Configure("Your_App_Id");
+			manager.DisableUpdateManager = false;
+			manager.StartManager();
 
 			return true;
 		}
