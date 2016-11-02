72ea9108a97bfd0234723965815d26455cba8cfa
Aare Undo <aare@carto.com>
2016-11-02 17:09:39 +0200

    Shared.Droid.iOS: removed unused VectorBaseMap

--- AdvancedMap.Droid/Sections/OfflineMap/PackageManager/PackagedMapActivity.cs
+++ AdvancedMap.Droid/Sections/OfflineMap/PackageManager/PackagedMapActivity.cs
@@ -6,25 +6,13 @@ using Shared.Droid;
 namespace AdvancedMap.Droid
 {
 	[Activity]
-	public class PackagedMapActivity : VectorBaseMapActivity
+	public class PackagedMapActivity : MapBaseActivity
 	{
 		protected override void OnCreate(Android.OS.Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
-
-			MapView.SetZoom(3, 0);
 		}
 
-		protected override Carto.DataSources.TileDataSource CreateTileDataSource()
-		{
-			// Using static global variable to pass data. Bad style, avoid this pattern in your apps     
-			if (PackageManagerActivity.DataSource != null)
-			{
-				return PackageManagerActivity.DataSource;
-			}
-
-			return base.CreateTileDataSource();
-		}
 	}
 }
 
