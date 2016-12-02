e90dd7ad0b37d0ebf7ae5f53110bb3d78b6d3038
Aare Undo <aare@carto.com>
2016-12-02 12:15:26 +0200

    nuget package update script

---
+++ update-package.py
@@ -0,0 +1,81 @@
+import sys
+from xml.dom import minidom
+
+# Used to automate .apk build process on Jenkins and upload to HockeyApp
+# Nuget does not feature a decent internal tool to update packages
+# Changes the version in a project's .csproj file and its packages.config,
+
+# Arguments: 
+# (1) .csproj path
+# (2) packages.config path
+# (3) version name/number (semantic)
+
+# Sample usage:
+# python update-package.py projects/mobile-dotnet-samples/AdvancedMap.Droid/AdvancedMap.Droid.csproj projects/mobile-dotnet-samples/AdvancedMap.Droid/packages.config 4.0.0-pre-170
+
+# Disclaimer:
+# This goes hand-in-hand with nuget.exe's package restore, 
+# else the numbers will be updated, but packages will not be restored.
+# Intended to work with CARTO's mobile-dotnet-samples and has not been tested on other projects.
+# Use at your own risk.
+# Additionally, it changes the spacing of your files
+
+SDKNAME = "CartoMobileSDK.Android"
+
+csprojFile = sys.argv[1]
+packageFile = sys.argv[2]
+version = sys.argv[3]
+
+print "Updating: " + csprojFile;
+
+xmldoc = minidom.parse(csprojFile)
+references = xmldoc.getElementsByTagName("Reference")
+
+found = False;
+
+for reference in references:
+    if reference.attributes["Include"].value == SDKNAME:
+    	
+    	hintpath = reference.getElementsByTagName("HintPath")[0];
+    	split = hintpath.firstChild.nodeValue.split("\\")
+
+    	base = split[0] + "\\" + split[1] + "\\"
+    	end = "\\" + split[3] + "\\" + split[4] + "\\" + split[5]
+    	hintpath.firstChild.nodeValue = base + SDKNAME + version + end
+
+    	found = True;
+
+if not found:
+	print "Couldn't find SDK reference in .csproj"
+	quit()
+
+xmldoc.writexml(open(csprojFile, 'w'), indent="", addindent="", newl='\n')
+
+print "Updating: " + packageFile;
+
+xmldoc = minidom.parse(packageFile)
+packages = xmldoc.getElementsByTagName("package")
+
+found = False;
+
+for package in packages:
+	if package.attributes["id"].value == SDKNAME:
+		package.attributes["version"] = version
+		found = True
+
+if not found:
+	print "Couldn't find SDK reference in packages.config"
+	quit()
+
+xmldoc.writexml(open(packageFile, 'w'), indent="", addindent="", newl='\n')
+
+
+
+
+
+
+
+
+
+
+
