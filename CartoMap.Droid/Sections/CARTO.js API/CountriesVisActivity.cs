134c5a2ca231db9cd8f7a944959964e1ff176a58
Aare Undo <aare@carto.com>
2017-01-06 16:02:37 +0200

    CartoMap: new/better vis json urls (unimplemented)

--- CartoMap.Droid/Sections/CARTO.js API/CountriesVisActivity.cs
+++ CartoMap.Droid/Sections/CARTO.js API/CountriesVisActivity.cs
@@ -15,6 +15,15 @@ namespace CartoMap.Droid
 			base.OnCreate(savedInstanceState);
 
 			UpdateVis("http://documentation.cartodb.com/api/v2/viz/2b13c956-e7c1-11e2-806b-5404a6a683d5/viz.json");
+
+			// Train: 
+			// UpdateVis"(https://mamataakella.cartodb.com/api/v2/viz/30730478-bbb5-11e5-b75c-0e5db1731f59/viz.json");
+
+			// Predicted store location: 
+			//UpdateVis("https://maps-for-all.cartodb.com/api/v2/viz/78b33d4a-3dd6-11e6-8632-0ea31932ec1d/viz.json");
+
+			// Telco:
+			//UpdateVis("https://raw.githubusercontent.com/CartoDB/labs-telco-insights/gh-pages/telco.viz.json");
 		}
 	}
 }
