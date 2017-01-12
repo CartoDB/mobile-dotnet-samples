fd4957ea9f4cfaa9329d00110550ecded9d9d1b9
Aare Undo <aare@carto.com>
2017-01-12 15:39:40 +0200

    HelloMap.Forms: initial commit

---
+++ HelloMap.Forms/HelloMap.Forms/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Carto.Ui;
+using Xamarin.Forms;
+
+namespace HelloMap.Forms
+{
+	public partial class App : Application
+	{
+#if __ANDROID__
+		const string License = "XTUN3Q0ZDQ1ZEcWc0clhPNENHQVFCOTUwQzNERUhEWERBaFIrMEZ2SExxait4dFN5ZzVNU0ZGeTgxMkMzaFE9PQoKYXBwVG9rZW49MDlkMmE2ZTgtMTA0ZC00Y2U0LWI2ODEtMDBkMjQ0NGY1MDIwCnBhY2thZ2VOYW1lPWNvbS5jYXJ0by5oZWxsb21hcF9mb3JtcwpvbmxpbmVMaWNlbnNlPTEKcHJvZHVjdHM9c2RrLXhhbWFyaW4tYW5kcm9pZC00LioKd2F0ZXJtYXJrPWN1c3RvbQo=";
+#else
+		const string License = "XTUMwQ0ZHMXBvYVk0cE95RHYwb05iMkNKaldkYklSdHVBaFVBeGRJamVhQzltaGZ0WlJ3bTFuVCt1cVkwSXk4PQoKYXBwVG9rZW49M2Q0MzQ0ZmYtZjczZi00NTZhLTk5YmMtOGE2ODE0MWViOTQ1CmJ1bmRsZUlkZW50aWZpZXI9Y29tLmNhcnRvLmhlbGxvbWFwLWZvcm1zCm9ubGluZUxpY2Vuc2U9MQpwcm9kdWN0cz1zZGsteGFtYXJpbi1pb3MtNC4qCndhdGVybWFyaz1jdXN0b20K";
+#endif
+
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new MainPage();
+
+#if __ANDROID__
+			MapView.RegisterLicense(License, Xamarin.Forms.Forms.Context);
+#else
+			MapView.RegisterLicense(License);
+#endif
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+}
+}
---
+++ HelloMap.Forms/HelloMap.Forms/Extensions.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using Carto.Core;
+using Carto.DataSources;
+using Carto.Layers;
+using Carto.Projections;
+using Carto.Styles;
+using Carto.Ui;
+using Carto.VectorElements;
+
+namespace HelloMap.Forms
+{
+	public static class Extensions
+	{
+		public static Marker AddMarkerToPosition(this MapView map, MapPos position)
+		{
+			// Initialize a local vector data source
+			Projection projection = map.Options.BaseProjection;
+			LocalVectorDataSource datasource = new LocalVectorDataSource(projection);
+
+			// Initialize a vector layer with the previous data source
+			VectorLayer layer = new VectorLayer(datasource);
+
+			// Add layer to map
+			map.Layers.Add(layer);
+
+			// Set marker style
+			MarkerStyleBuilder builder = new MarkerStyleBuilder();
+			builder.Size = 15;
+			builder.Color = new Carto.Graphics.Color(0, 255, 0, 255);
+
+			MarkerStyle style = builder.BuildStyle();
+
+			// Create marker and add it to the source
+			Marker marker = new Marker(position, style);
+			datasource.Add(marker);
+
+			return marker;
+		}
+	}
+}
