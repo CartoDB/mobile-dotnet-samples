327c75538c8e2f8d42c24365bf0c5675ebe8d43b
Aare Undo <aare@carto.com>
2017-05-04 10:50:43 +0300

    iOS: refactor; ScaleAspectFill and ClipsToBounds image

--- mobile_dotnet_samples/Views/Gallery/MapGalleryView.cs
+++ mobile_dotnet_samples/Views/Gallery/MapGalleryView.cs
@@ -85,57 +85,6 @@ namespace Shared.iOS
 				rows.Add(row);
 			}
 		}
-	}
 
-	public class GalleryRow : UIView
-	{
-		UIImageView image;
-		UILabel label;
-
-		public GalleryRow(MapListRowSource source)
-		{
-			BackgroundColor = Colors.CartoRed;
-
-			image = new UIImageView();
-			image.Image = UIImage.FromFile(source.ImageResource);
-			image.ContentMode = UIViewContentMode.ScaleToFill;
-
-			label = new UILabel();
-			label.Text = source.Title.ToUpper();
-			label.Font = UIFont.FromName("HelveticaNeue", 10);
-			label.TextColor = UIColor.White;
-			label.TextAlignment = UITextAlignment.Center;
-
-			AddSubviews(image, label);
-
-		}
-
-		public override void LayoutSubviews()
-		{
-			base.LayoutSubviews();
-
-			UIBezierPath path = UIBezierPath.FromRect(Bounds);
-			Layer.MasksToBounds = false;
-			Layer.ShadowColor = Colors.CartoNavy.CGColor;
-			Layer.ShadowOffset = new CGSize(2.0f, 2.0f);
-			Layer.ShadowOpacity = 0.5f;
-			Layer.ShadowPath = path.CGPath;
-
-			nfloat padding = 5;
-
-			nfloat labelHeight = (Frame.Height - 3 * padding) / 4;
-
-			nfloat x = padding;
-			nfloat y = padding;
-			nfloat w = Frame.Width - 2 * padding;
-			nfloat h = Frame.Height - labelHeight;
-
-			image.Frame = new CGRect(x, y, w, h);
-
-			y += h - padding / 2;
-			h = labelHeight;
-
-			label.Frame = new CGRect(x, y, w, h);
-		}
 	}
 }
