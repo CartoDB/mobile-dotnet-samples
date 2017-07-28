28db722a71f0ff223d75c3a3446e6b3688f8c801
Aare Undo <aare@carto.com>
2017-07-28 17:28:19 +0300

    Shared: Geocoding.cs; GeocodingExtensions

---
+++ Shared/Geocoding/GeocodingExtensions.cs
@@ -0,0 +1,78 @@
+﻿
+using System;
+using Carto.Geocoding;
+
+namespace Shared
+{
+    public static class GeocodingExtensions
+    {
+        public static string GetPrettyAddress(this GeocodingResult result)
+        {
+            var parsed = "";
+            var address = result.Address;
+
+            if (address.Name.IsNotEmpty())
+            {
+                parsed += address.Name;
+            }
+
+            if (address.Street.IsNotEmpty())
+            {
+                parsed += parsed.AddCommaIfNecessary();
+                parsed += address.Street;
+            }
+
+            if (address.HouseNumber.IsNotEmpty())
+            {
+                parsed += " " + address.HouseNumber;
+            }
+
+            if (address.Neighbourhood.IsNotEmpty())
+            {
+                parsed += parsed.AddCommaIfNecessary();
+                parsed += address.Neighbourhood;
+            }
+
+            if (address.Locality.IsNotEmpty())
+            {
+                parsed += parsed.AddCommaIfNecessary();
+                parsed += address.Locality;
+            }
+
+            if (address.County.IsNotEmpty())
+            {
+                parsed += parsed.AddCommaIfNecessary();
+                parsed += address.County;
+            }
+
+            if (address.Region.IsNotEmpty())
+            {
+                parsed += parsed.AddCommaIfNecessary();
+                parsed += address.Region;
+            }
+
+            if (address.Country.IsNotEmpty())
+            {
+                parsed += parsed.AddCommaIfNecessary();
+                parsed += address.Country;
+            }
+
+            return parsed;
+        }
+
+        public static string AddCommaIfNecessary(this string original)
+        {
+            if (original.Length > 0)
+            {
+                return ", ";
+            }
+
+            return "";
+        }
+
+        public static bool IsNotEmpty(this string original)
+        {
+            return !string.IsNullOrWhiteSpace(original);
+        }
+    }
+}
