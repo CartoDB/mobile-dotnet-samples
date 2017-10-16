c49a3d9a22aec20b75be950a6bf030a9cd6db73a
Aare Undo <aare@carto.com>
2017-10-16 11:33:59 +0300

    Advanced.iOS: new location marker

--- AdvancedMap.iOS/Sections/Other/GpsLocationMapController.cs
+++ AdvancedMap.iOS/Sections/Other/GpsLocationMapController.cs
@@ -9,6 +9,7 @@ using Carto.VectorElements;
 using CoreLocation;
 using Shared;
 using Shared.iOS;
+using Shared.Utils;
 using UIKit;
 
 namespace AdvancedMap.iOS
@@ -19,80 +20,41 @@ namespace AdvancedMap.iOS
 
 		public override string Description { get { return "Shows user GPS location on map"; } }
 
-		LocationManager LocationManager { get; set; }
+        LocationManager manager;
 
-		Marker positionMarker;
-		LocalVectorDataSource markerSource;
-		BalloonPopup markerLabel;
-
-		bool IsMarkerSet { get { return positionMarker != null; } }
+        LocationMarker marker;
 
 		public override void ViewDidLoad()
 		{
 			base.ViewDidLoad();
 
             // Add default base layer
-            //AddOnlineBaseLayer(CartoBaseMapStyle.CartoBasemapStyleVoyager);
-            MapView.BackgroundColor = UIColor.Yellow;
-			LocationManager = new LocationManager();
-			LocationManager.Start();
+            AddOnlineBaseLayer(CartoBaseMapStyle.CartoBasemapStyleVoyager);
+			manager = new LocationManager();
 
-			// Create layer and add object to the layer, finally add layer to the map. 
-			// All overlay layers must be same projection as base layer, so we reuse it
-			markerSource = new LocalVectorDataSource(MapView.Options.BaseProjection);
-			var _markerLayer = new VectorLayer(markerSource);
-			MapView.Layers.Add(_markerLayer);
+            marker = new LocationMarker(MapView);
 		}	
 
 		public override void ViewWillAppear(bool animated)
 		{
 			base.ViewWillAppear(animated);
 
-			LocationManager.LocationUpdated += OnLocationUpdate;
+            manager.Start();
+			manager.LocationUpdated += OnLocationUpdate;
 		}
 
 		public override void ViewWillDisappear(bool animated)
 		{
 			base.ViewWillDisappear(animated);
 
-			LocationManager.LocationUpdated -= OnLocationUpdate;
+			manager.LocationUpdated -= OnLocationUpdate;
 		}
 
 		void OnLocationUpdate(object sender, LocationUpdatedEventArgs e)
 		{
-			double latitude = e.Location.Coordinate.Latitude;
-			double longitude = e.Location.Coordinate.Longitude;
-
-			string title = "Your current location";
-			string description = latitude.To4Decimals() + ", " + longitude.To4Decimals();
-
-			MapPos location = MapView.Options.BaseProjection.FromWgs84(new MapPos(longitude, latitude));
-
-			if (IsMarkerSet) {
-				markerLabel.Description = description;
-				positionMarker.Geometry = new PointGeometry(location);
-				return;
-			}
-
-			// Load default market style
-			MarkerStyleBuilder markerStyleBuilder = new MarkerStyleBuilder();
-
-			// Add the label to the Marker
-			positionMarker = new Marker(location, markerStyleBuilder.BuildStyle());
-
-			// Define label what is shown when you click on marker, with default style
-			var builder = new BalloonPopupStyleBuilder();
-			markerLabel = new BalloonPopup(positionMarker, builder.BuildStyle(), title, description);
-
-			// Add the marker and label to the layer
-			markerSource.Add(positionMarker);
-			markerSource.Add(markerLabel);
-
-			// Center the map in the current location
-			MapView.FocusPos = location;
-
-			// Zoom in the map in the current location
-			MapView.Zoom = 19f;
+            var coordinate = e.Location.Coordinate;
+            var accuracy = (float)e.Location.HorizontalAccuracy;
+            marker.ShowAt(coordinate.Latitude, coordinate.Longitude, accuracy);
 		}
 
 	}
